f60889f3deb8aacaff8536c2db58602cc71ed456
ssithole1 <ssithole@sarao.ac.za>
2023-09-12 15:46:53 +0200

    start the gui at the same time with component manager

--- src/astt_gui/app.py
+++ src/astt_gui/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request
+
+from component_managers.astt_comp_manager import ASTTComponentManager
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def start_astt_gui():
+
+    # Start the  component manager simultaneusly with the gui
+    cm = ASTTComponentManager()
+    cm.connect_to_network()
+    node2 = cm.connect_to_plc_node()
+    cm.subscribe_to_az_change(node2)
+    cm.subscribe_to_el_change(node2)
+    # cm.trigger_transmission(node2) 
+
+    if request.method == 'POST':
+        az = request.form['azimuth']
+        el = request.form['elevation']
+        print(az, el)
+
     return render_template("index.html")
 
 
