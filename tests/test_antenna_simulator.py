922b66c75b9fbd33bc688f61d2d2f87aab04d260
alethole <letholematiba@gmail.com>
2023-09-13 18:50:13 +0200

    astt-59 import fix & availability test fix

--- tests/test_antenna_simulator.py
+++ tests/test_antenna_simulator.py
@@ -1,6 +1,5 @@
-from astt_cam_software.component_managers.astt_comp_manager import (
-    ASTTComponentManager,
-)
+"""Test for the Component manager."""
+from component_managers.astt_comp_manager import ASTTComponentManager
 
 cm = ASTTComponentManager()
 cm.connect_to_network()
@@ -8,16 +7,18 @@ node2 = cm.connect_to_plc_node()
 
 
 def test_antenna_sim_is_available():
-    (cm.network0).send_message(0x0, [0x1, 0])
-    node2.nmt.wait_for_heartbeat()
-    assert node2.nmt.state == "OPERATIONAL"
+    """Test if the simulator is alive through an sdo request."""
+    node2.sdo["TPDO communication parameter"]["transmission type"].raw
+    assert node2.nmt.state == "INITIALISING"
 
 
 def test_sim_trans_to_pre_operational():
+    """Tests if the simulator can transitition to PRE OPERA.. """
     cm.set_plc_node_to_preoperational(node2)
     assert node2.nmt.state == "PRE-OPERATIONAL"
 
 
 def test_sim_trans_to_operational():
+    """Tests if the simulator can transitition to OPERA.. """
     cm.set_plc_node_to_operational(node2)
     assert node2.nmt.state == "OPERATIONAL"
