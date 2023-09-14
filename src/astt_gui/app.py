95d58f803d9ff764250388b8cc91b573d1890795
alethole <letholematiba@gmail.com>
2023-09-14 10:44:32 +0200

    astt-52 Moved the submit button to be under the form

--- src/astt_gui/app.py
+++ src/astt_gui/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 
 from component_managers.astt_comp_manager import ASTTComponentManager
@@ -6,20 +5,20 @@ from component_managers.astt_comp_manager import ASTTComponentManager
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def start_astt_gui():
-
     # Start the  component manager simultaneusly with the gui
     cm = ASTTComponentManager()
     cm.connect_to_network()
     node2 = cm.connect_to_plc_node()
     cm.subscribe_to_az_change(node2)
     cm.subscribe_to_el_change(node2)
-    # cm.trigger_transmission(node2) 
+    # cm.trigger_transmission(node2)
 
-    if request.method == 'POST':
-        az = request.form['azimuth']
-        el = request.form['elevation']
+    if request.method == "POST":
+        print("POST METHOD!!")
+        az = request.form["azimuth"]
+        el = request.form["elevation"]
         print(az, el)
 
     return render_template("index.html")
