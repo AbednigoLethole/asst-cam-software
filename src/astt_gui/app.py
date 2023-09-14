b34cc4da2ad422c32c64ede7ff94aa664425f499
ssithole1 <ssithole@sarao.ac.za>
2023-09-14 12:50:37 +0200

    validated the input fields

--- src/astt_gui/app.py
+++ src/astt_gui/app.py
@@ -1,9 +1,12 @@
+import time
 from flask import Flask, render_template, request
 
 from component_managers.astt_comp_manager import ASTTComponentManager
 
-app = Flask(__name__)
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
+
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def start_astt_gui():
@@ -13,13 +16,14 @@ def start_astt_gui():
     node2 = cm.connect_to_plc_node()
     cm.subscribe_to_az_change(node2)
     cm.subscribe_to_el_change(node2)
-    # cm.trigger_transmission(node2)
 
     if request.method == "POST":
-        print("POST METHOD!!")
         az = request.form["azimuth"]
         el = request.form["elevation"]
-        print(az, el)
+
+        cm.point_to_coordinates(node2,int(time.time()),float(az),float(el))
+        cm.trigger_transmission(node2)
+
 
     return render_template("index.html")
 
