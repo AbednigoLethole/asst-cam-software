5da11de085db2d72f1178f724e3de2f837752c3d
phelokazi_dube <pdube@ska.ac.za>
2023-09-19 16:50:12 +0200

    ASTT-56 Fixed linting error

--- src/astt_gui/app.py
+++ src/astt_gui/app.py
@@ -11,8 +11,8 @@ cm = ASTTComponentManager()
 
 @app.route("/", methods=["GET"])
 def index():
-
     return render_template("index.html")
+
 
 @app.route("/", methods=["POST"])
 def start_astt_gui():
