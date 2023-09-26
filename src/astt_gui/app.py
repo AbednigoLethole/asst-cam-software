e80786e1e880411b004fcd6cfb2b65197bb6abbf
alethole <letholematiba@gmail.com>
2023-09-26 17:38:20 +0200

    ASTT-56 Hooked initialize button with func to start Simulator.

--- src/astt_gui/app.py
+++ src/astt_gui/app.py
@@ -7,6 +7,7 @@ from component_managers.start_simulator import SimulatorManager
 
 app = Flask(__name__)
 cm = ASTTComponentManager()
+node2 = None
 
 
 @app.route("/", methods=["GET"])
@@ -16,21 +17,40 @@ def index():
 
 @app.route("/", methods=["POST"])
 def start_astt_gui():
-    az = request.form["azimuth"]
-    el = request.form["elevation"]
-    cm.point_to_coordinates(
-        node2, int(time.time()), float(az), float(el)
-    )
-    cm.trigger_transmission(node2)
+    # Trigger condition when Initialize button is clicked.
+
+    if (
+        "button" in request.form
+        and request.form["button"] == "Initialize"
+    ):
+        # Start VCAN network & simulator
+        simulator_manager = SimulatorManager()
+        simulator_manager.start_can_interface()
+        simulator_manager.run_contaier_and_startup_simulator()
+        # Await Simulator to start up
+        time.sleep(3)
+        # Connect to VCAN and Siumlator
+        cm.connect_to_network()
+        global node2
+        node2 = cm.connect_to_plc_node()
+        # Subscribe to AZ and EL change.
+        cm.subscribe_to_az_change(node2)
+        cm.subscribe_to_el_change(node2)
+
+    # Trigger condition when Point button is clicked.
+    if "azimuth" in request.form and "elevation" in request.form:
+        # Get AZ and EL from GUI.
+        az = request.form["azimuth"]
+        el = request.form["elevation"]
+        # Call a method to point to Desired AZ & EL
+        cm.point_to_coordinates(
+            node2, int(time.time()), float(az), float(el)
+        )
+        # Display current AZ and EL.
+        cm.trigger_transmission(node2)
+
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    simulator_manager = SimulatorManager()
-    simulator_manager.start_can_interface()
-    simulator_manager.run_contaier_and_startup_simulator()
-    cm.connect_to_network()
-    node2 = cm.connect_to_plc_node()
-    cm.subscribe_to_az_change(node2)
-    cm.subscribe_to_el_change(node2)
     app.run()
