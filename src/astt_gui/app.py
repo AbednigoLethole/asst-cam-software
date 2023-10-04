9b353660802a3b5ca94cd673cc9524f4a6f1667b
ssithole1 <ssithole@sarao.ac.za>
2023-10-04 22:04:34 +0200

    added az and el position and graph on gui

--- src/astt_gui/app.py
+++ src/astt_gui/app.py
@@ -1,6 +1,10 @@
 import time
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+import random
+import json
+from time import time
+from random import random
 
 from component_managers.astt_comp_manager import ASTTComponentManager
 from component_managers.start_simulator import SimulatorManager
@@ -19,10 +23,7 @@ def index():
 def start_astt_gui():
     # Trigger condition when Initialize button is clicked.
 
-    if (
-        "button" in request.form
-        and request.form["button"] == "Initialize"
-    ):
+    if "button" in request.form and request.form["button"] == "Initialize":
         # Start VCAN network & simulator
         simulator_manager = SimulatorManager()
         simulator_manager.start_can_interface()
@@ -43,13 +44,28 @@ def start_astt_gui():
         az = request.form["azimuth"]
         el = request.form["elevation"]
         # Call a method to point to Desired AZ & EL
-        cm.point_to_coordinates(
-            node2, int(time.time()), float(az), float(el)
-        )
+        cm.point_to_coordinates(node2, int(time.time()), float(az), float(el))
         # Display current AZ and EL.
         cm.trigger_transmission(node2)
 
     return render_template("index.html")
+
+
+@app.route("/data", methods=["GET", "POST"])
+def data():
+    # Data Format
+    # [TIME, Temperature, Humidity]
+
+    Azimuth = random() * 100
+    Elevation = random() * 55
+
+    data = [time() * 1000, Azimuth, Elevation]
+
+    response = make_response(json.dumps(data))
+
+    response.content_type = "application/json"
+
+    return response
 
 
 if __name__ == "__main__":
