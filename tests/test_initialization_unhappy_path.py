49a68a534777d87fdef25d7d8abcd2b5cfd5edc2
alethole <letholematiba@gmail.com>
2023-11-29 18:28:47 +0200

    ASTT-71 Removed unneccessary code and updated command to start the simulator.

--- tests/test_initialization_unhappy_path.py
+++
@@ -1,8 +0,0 @@
-from component_managers.start_simulator import SimulatorManager
-
-
-def test_start_can_interface_unhappy_path():
-    # Test against the unhappy path
-    sm = SimulatorManager()
-    result = sm.start_can_interface("123")
-    assert result == 1
