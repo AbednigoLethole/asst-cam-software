628cf83bbb05417387acfcbb38ec94b5127b19e1
alethole <letholematiba@gmail.com>
2024-01-16 16:19:55 +0200

    ASTT-22 Added calls to connect to the antenna

--- tests/acceptance/test_modes_and_states.py
+++ tests/acceptance/test_modes_and_states.py
@@ -3,4 +3,6 @@
 
 def test_antenna_starts_at_idle(antenna_comp_manager):
     """Test initial state of the simulator"""
+    antenna_comp_manager.connect_to_network()
+    antenna_comp_manager.connect_to_plc_node()
     assert antenna_comp_manager.antenna_mode() == 0
