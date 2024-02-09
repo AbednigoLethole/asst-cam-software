3828e8222ab26e0f2c82396b711a4f11a1ee4243
Suprise Sithole <ssithole@sarao.ac.za>
2024-02-09 12:12:47 +0200

    state colors added

--- src/astt_gui/static/js/states.js
+++ src/astt_gui/static/js/states.js
@@ -1,3 +1,4 @@
+//////////////////////Mode states///////////////////////////////
 function modeChanged(newMode) {
     var modeColor = "";
     var modeAndColour = {
@@ -11,10 +12,55 @@ function modeChanged(newMode) {
     }
 
     document.addEventListener("DOMContentLoaded", function() {
-        var modeButton = document.getElementById("Modes state");
+        var modeButton = document.getElementById("modeState");
 
         modeButton.style.backgroundColor = modeColor;
         modeButton.innerText = newMode;
     });
 }
+///////////////////////Stow pin State //////////////////////////
+
+function StowPinState(stowState) {
+    var stateColor = "";
+    var stowStateAndColour = {
+        "Unknown": "#212529",
+        "StowPinEngaged": "#dc3545",
+        "StowPinReleased": "#198754"
+    };
+    if (stowState in stowStateAndColour) {
+        stateColor = stowStateAndColour[stowState];
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var stateButton = document.getElementById("funcState");
+
+        stateButton.style.backgroundColor = stateColor;
+        stateButton.innerText = stowState;
+    });
+}
+
+/////////////////////Function State //////////////////////////////
+
+function funcState(funcState) {
+    var stateColor = "";
+    var funcStateAndColour = {
+        "Unknown": "#212529",
+        "Braked": "#dc3545",
+        "Moving": "#198754"
+    };
+    if (funcState in funcStateAndColour) {
+        stateColor = funcStateAndColour[funcState];
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var stateButton = document.getElementById("funcState");
+
+        stateButton.style.backgroundColor = stateColor;
+        stateButton.innerText = funcState;
+    });
+}
+
+StowPinState("StowPinReleased")
+modeChanged("Idle")
+funcState("Braked")
 
