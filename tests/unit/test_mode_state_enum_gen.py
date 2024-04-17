7f0b73fc861ed343d3a5b2ef42459b310677c5ae
alethole <letholematiba@gmail.com>
2024-04-17 22:15:33 +0200

    ASTT-93 Reworked the docstring

--- tests/unit/test_mode_state_enum_gen.py
+++ tests/unit/test_mode_state_enum_gen.py
@@ -13,31 +13,41 @@ class TestStatesModesGeneratiom(unittest.TestCase):
         self.manager = ASTTComponentManager()
 
     def test_generated_mode_with_valid_input(self):
-        """Use a valid value to test the creation of the mode."""
+        """
+        Use a valid value to test the creation of the mode.
+        """
         generated_enum = self.manager.gen_mode_state_enums("Mode", 1)
         assert generated_enum.name == "POINT"
 
     def test_generated_mode_with_invalid_input(self):
-        """Use an invalid value to test the creation of the mode."""
+        """
+        Use an invalid value to test the creation of the mode.
+        """
         generated_enum = self.manager.gen_mode_state_enums("Mode", 6)
         assert generated_enum is None
 
     def test_generated_func_state_with_valid_input(self):
-        """Use a valid value to test the creation of the func state."""
+        """
+        Use a valid value to test the creation of the func state.
+        """
         generated_enum = self.manager.gen_mode_state_enums(
             "FuncState", 0
         )
         assert generated_enum.name == "BRAKED"
 
     def test_generated_func_state_invalid_input(self):
-        """Use an invalid value to test the creation of the func state."""
+        """
+        Use an invalid value to test the creation of the func state.
+        """
         generated_enum = self.manager.gen_mode_state_enums(
             "FuncState", 10
         )
         assert generated_enum is None
 
     def test_generated_stow_state_with_valid_input(self):
-        """Use a valid value to test the creation of the stow state."""
+        """
+        Use a valid value to test the creation of the stow state.
+        """
         generated_enum = self.manager.gen_mode_state_enums(
             "StowPinState", 4
         )
@@ -47,7 +57,9 @@ class TestStatesModesGeneratiom(unittest.TestCase):
         )
 
     def test_generated_stow_state_invalid_input(self):
-        """Use an invalid value to test the creation of the stow state."""
+        """
+        Use an invalid value to test the creation of the stow state.
+        """
         generated_enum = self.manager.gen_mode_state_enums(
             "StowPinState", 13
         )
