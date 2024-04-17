975baf7b6f96705e6e7749f8f62e4610df6f0709
alethole <letholematiba@gmail.com>
2024-04-17 21:55:19 +0200

    ASTT-93 Paraphrased the docstrings

--- tests/unit/test_mode_state_enum_gen.py
+++ tests/unit/test_mode_state_enum_gen.py
@@ -1,5 +1,4 @@
 """ Tests for the generation of mode & states enums"""
-
 import unittest
 
 from src.component_managers.astt_comp_manager import (
@@ -13,44 +12,33 @@ class TestStatesModesGeneratiom(unittest.TestCase):
         self.manager = ASTTComponentManager()
 
     def test_generated_mode_with_valid_input(self):
-        """Test out the generation of antenna mode using valid enum value"""
-        generated_enum = self.manager.gen_mode_state_enums("Mode", 1)
+        """Use a valid value to test the creation of the mode."""
+        generated_enum = self.manager.gen_mode_state_enums("Mode",1)
         assert generated_enum.name == "POINT"
 
     def test_generated_mode_with_invalid_input(self):
-        """Test out the generation of antenna mode using valid enum value"""
-        generated_enum = self.manager.gen_mode_state_enums("Mode", 6)
+        """Use an invalid value to test the creation of the mode."""
+        generated_enum = self.manager.gen_mode_state_enums("Mode",6)
         assert generated_enum is None
 
     def test_generated_func_state_with_valid_input(self):
-        """Test out the generation of func state using valid enum value"""
-        generated_enum = self.manager.gen_mode_state_enums(
-            "FuncState", 0
-        )
+        """Use a valid value to test the creation of the func state."""
+        generated_enum = self.manager.gen_mode_state_enums("FuncState",0)
         assert generated_enum.name == "BRAKED"
 
     def test_generated_func_state_invalid_input(self):
-        """Test out the generation of antenna func state using valid enum value"""
-        generated_enum = self.manager.gen_mode_state_enums(
-            "FuncState", 10
-        )
+        """Use an invalid value to test the creation of the func state."""
+        generated_enum = self.manager.gen_mode_state_enums("FuncState",10)
         assert generated_enum is None
 
     def test_generated_stow_state_with_valid_input(self):
-        """Test out the generation of stow state using valid enum value"""
-        generated_enum = self.manager.gen_mode_state_enums(
-            "StowPinState", 4
-        )
-        assert (
-            generated_enum.name
-            == "ENGAGED_NOT_RELEASED_NOT_STOW_WINDOW"
-        )
+        """Use a valid value to test the creation of the stow state."""
+        generated_enum = self.manager.gen_mode_state_enums("StowPinState",4)
+        assert generated_enum.name == "ENGAGED_NOT_RELEASED_NOT_STOW_WINDOW"
 
     def test_generated_stow_state_invalid_input(self):
-        """Test out the generation of stow state using valid enum value"""
-        generated_enum = self.manager.gen_mode_state_enums(
-            "StowPinState", 13
-        )
+        """Use an invalid value to test the creation of the stow state."""
+        generated_enum = self.manager.gen_mode_state_enums("StowPinState",13)
         assert generated_enum is None
 
 
