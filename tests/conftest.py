ad5042df5dda244b68b6de83eabcbb4cf11532df
Silere8 <usilere@sarao.ac.za>
2024-06-11 09:02:55 +0200

    ASTT-106 testing PAT

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,13 @@ import pytest
 from component_managers.astt_comp_manager import ASTTComponentManager
 
 
+
 @pytest.fixture(scope="module")
 def cm_manager_connected_to_antnn():
     """Estabilish connection and return the antenna node"""
+
+    # testing ASTT Token
+
     cm = ASTTComponentManager()
     cm.connect_to_network()
     cm.connect_to_plc_node()
