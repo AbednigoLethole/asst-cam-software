c0b37cb8e901e73ada6a2c4d0d1ec6ce4580a506
Silere8 <usilere@sarao.ac.za>
2024-08-16 12:45:18 +0200

    ASTT-106 disable W0511 warning

--- src/threading_lrc.py
+++ src/threading_lrc.py
@@ -9,8 +9,8 @@ def background(f):
     use @background above the function
     you want to run in the background
     """
-    # pylint : disable=W0511
-    # TODO:Introduce Dramatiq to control and monitor background threads.
+
+    # TODO:Introduce Dramatiq to control and monitor background threads. # pylint: disable=W0511
     def backgrnd_func(*a, **kw):
         threading.Thread(target=f, args=a, kwargs=kw).start()
 
